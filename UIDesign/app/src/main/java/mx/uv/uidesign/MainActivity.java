12ffbafca347c94f3024b389ce1a2820d200eb3f
joeljacp14 <87150153+joeljacp14@users.noreply.github.com>
2021-09-08 02:44:48 -0500

    Add files via upload

---
+++ UIDesign/app/src/main/java/mx/uv/uidesign/MainActivity.java
@@ -0,0 +1,18 @@
+package mx.uv.uidesign;
+
+/*
+* @authors:
+* Oskar Ramses Beltran Magaña
+* Joel Jacome Pioquinto
+* */
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+}
